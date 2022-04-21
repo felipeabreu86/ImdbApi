e40e5085904e190e220d5d8e8bfea9d262144db4
Felipe Abreu <felipeabreu86@users.noreply.github.com>
2022-04-21 16:31:01 -0300

    First Commit

---
+++ ImdbApi/src/br/com/alura/imdb/ImdbApiRequest.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.com.alura.imdb;
+
+/**
+ * @author Felipe Abreu
+ *
+ */
+public class ImdbApiRequest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Imbd Api");
+	}
+}
